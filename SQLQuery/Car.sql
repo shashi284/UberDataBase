bfb9a676ae6bf1394c5b216e77b43df4a1c6c3b5
Shashi <shashishekhar284@gmail.com>
2019-11-26 17:48:20 -0600

    Tables

--- SQLQuery/Car.sql
+++ SQLQuery/Car.sql
@@ -1,8 +1,8 @@
 CREATE TABLE CAR_DETAILS
 (   REGISTRATION_NUMBER varchar2(50) not null,
-    COLOR varchar2(50),
-    MODEL_NUMBER varchar2(50),
-    MODEL numeric(10),
+    COLOR varchar2(50),--RED BLUE
+    CAR_NAME varchar2(50),--FORD,CHEVROLET
+    CAR_TYPE varchar2(10),--X,XL
     CONSTRAINT CAR_PRIMARY_KEY  PRIMARY KEY (REGISTRATION_NUMBER)
 );
 
